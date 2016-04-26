0c49387341d85a8e150ccaca3fdc8137e1962e01
Dinah Bakeer <dinah.bakeer@gmail.com>
2016-04-26 19:38:37 -0400

    white link for ticket section

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,5 +1,43 @@
 fullpage.initialize('#fullpage', {
-  anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+  anchors: ['feteharriett', 'hosts', 'info', 'tickets', 'location'],
   menu: '#menu',
-  css3:true
+  css3:true,
+  scrollbar: true,
+  onLeave: function(index, nextIndex, direction) {
+     console.log(index, nextIndex, direction);
+     if (index == 3 && direction == 'down') {
+       $('header a').css('color', 'white');
+     }
+     else if (index == 5 && direction == 'up') {
+       $('header a').css('color', 'white');
+     }
+     else if (index == 1 && nextIndex == 4 && direction == 'down') {
+       $('header a').css('color', 'white');
+     }
+     else if (index == 2 && nextIndex == 4 && direction == 'down') {
+       $('header a').css('color', 'white');
+     }
+     else {
+       $('header a').css('color', 'black');
+     }
+  }
+  // navigation: true
 });
+
+
+
+
+// function initMap() {
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: 0, lng: 0},
+//     zoom: 3,
+//     styles: [{
+//       featureType: 'poi',
+//       stylers: [{ visibility: 'off' }]  // turn off points of interest.
+//     }, {
+//       featureType: 'transit.station',
+//       stylers: [{ visibility: 'off' }]  // Turn off bus stations, train stations, etc.
+//     }],
+//     disableDoubleClickZoom: true
+//   });
+// }
