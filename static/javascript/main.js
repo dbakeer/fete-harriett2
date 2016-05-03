73d317ddef8499f05d06baa83c828a98c3ba52a0
Dinah Bakeer <dinah.bakeer@gmail.com>
2016-05-03 11:35:36 -0400

    cleaned up css and reinstalled font

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -12,7 +12,6 @@ $(window).on('load', function(){
 });
 
 $(document).ready(function() {
-  if(screen.width > 480) {
   $('#fullpage').fullpage({
     anchors: ['feteharriett', 'hosts', 'info', 'tickets', 'location', 'contact'],
     menu: '#menu',
@@ -49,7 +48,6 @@ $(document).ready(function() {
       }
     }
   });
-  }
 });
 
 jQuery('img.svg').each(function(){
