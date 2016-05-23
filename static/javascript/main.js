c1e5fe27ee30b43aec97b26f84f3e59775f7075b
Dinah Bakeer <dinah.bakeer@gmail.com>
2016-05-23 12:04:17 -0400

    adding main site

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
-  var $header = $('header');
+  var header = $('header');
 
-  $('#fullpage').fullpage({
-    anchors: ['feteharriett', 'info', 'mission', 'hosts', 'tickets', 'location', 'contact'],
-    menu: '#menu',
+  $('#fullpage-main').fullpage({
+    anchors: ['feteharriett', 'event', 'about', 'tickets', 'auction', 'location', 'donors', 'contact'],
+    menu: '#main-menu',
     responsiveHeight: 1000,
     // css3:true,
     // autoScrolling: false,
@@ -12,53 +12,53 @@ $(document).ready(function() {
     // normalScrollElements: ".section",
     // scrollBar: false,
     // fitToSection: true,
-    onLeave: function(index, nextIndex, direction) {
-      if (index == 4 && direction == 'down') {
-        $('header a').css('color', '#f2f0ed');
-        $('header li').css('color', '#f2f0ed');
-      } else if (index == 6 && direction == 'up') {
-        $('header a').css('color', '#f2f0ed');
-        $('header li').css('color', '#f2f0ed');
-      } else if (index == 1 && nextIndex == 5 && direction == 'down') {
-        $('header a').css('color', '#f2f0ed');
-        $('header li').css('color', '#f2f0ed');
-      } else if (index == 2 && nextIndex == 5 && direction == 'down') {
-        $('header a').css('color', '#f2f0ed');
-        $('header li').css('color', '#f2f0ed');
-      } else if (index == 7 && nextIndex == 5 && direction == 'up') {
-        $('header a').css('color', '#f2f0ed');
-        $('header li').css('color', '#f2f0ed');
-      } else if (index == 4 && direction == 'up') {
-        $('header a').css('color', '#f2f0ed');
-        $('header li').css('color', '#f2f0ed');
-      } else if (index == 1 && nextIndex == 3 && direction == 'down') {
-        $('header a').css('color', '#f2f0ed');
-        $('header li').css('color', '#f2f0ed');
-      } else if (index == 2 && nextIndex == 3 && direction == 'down') {
-        $('header a').css('color', '#f2f0ed');
-        $('header li').css('color', '#f2f0ed');
-      } else if (index == 4 && nextIndex == 3 && direction == 'up') {
-        $('header a').css('color', '#f2f0ed');
-        $('header li').css('color', '#f2f0ed');
-      } else if (index == 5 && nextIndex == 3 && direction == 'up') {
-        $('header a').css('color', '#f2f0ed');
-        $('header li').css('color', '#f2f0ed');
-      } else if (index == 6 && nextIndex == 3 && direction == 'up') {
-        $('header a').css('color', '#f2f0ed');
-        $('header li').css('color', '#f2f0ed');
-      } else if (index == 7 && nextIndex == 3 && direction == 'up') {
-        $('header a').css('color', '#f2f0ed');
-        $('header li').css('color', '#f2f0ed');
-      } else {
-        $('header a').css('color', 'black');
-        $('header li').css('color', 'black');
-      }
-    },
+    // onLeave: function(index, nextIndex, direction) {
+    //   if (index == 4 && direction == 'down') {
+    //     $('header a').css('color', '#f2f0ed');
+    //     $('header li').css('color', '#f2f0ed');
+    //   } else if (index == 6 && direction == 'up') {
+    //     $('header a').css('color', '#f2f0ed');
+    //     $('header li').css('color', '#f2f0ed');
+    //   } else if (index == 1 && nextIndex == 5 && direction == 'down') {
+    //     $('header a').css('color', '#f2f0ed');
+    //     $('header li').css('color', '#f2f0ed');
+    //   } else if (index == 2 && nextIndex == 5 && direction == 'down') {
+    //     $('header a').css('color', '#f2f0ed');
+    //     $('header li').css('color', '#f2f0ed');
+    //   } else if (index == 7 && nextIndex == 5 && direction == 'up') {
+    //     $('header a').css('color', '#f2f0ed');
+    //     $('header li').css('color', '#f2f0ed');
+    //   } else if (index == 4 && direction == 'up') {
+    //     $('header a').css('color', '#f2f0ed');
+    //     $('header li').css('color', '#f2f0ed');
+    //   } else if (index == 1 && nextIndex == 3 && direction == 'down') {
+    //     $('header a').css('color', '#f2f0ed');
+    //     $('header li').css('color', '#f2f0ed');
+    //   } else if (index == 2 && nextIndex == 3 && direction == 'down') {
+    //     $('header a').css('color', '#f2f0ed');
+    //     $('header li').css('color', '#f2f0ed');
+    //   } else if (index == 4 && nextIndex == 3 && direction == 'up') {
+    //     $('header a').css('color', '#f2f0ed');
+    //     $('header li').css('color', '#f2f0ed');
+    //   } else if (index == 5 && nextIndex == 3 && direction == 'up') {
+    //     $('header a').css('color', '#f2f0ed');
+    //     $('header li').css('color', '#f2f0ed');
+    //   } else if (index == 6 && nextIndex == 3 && direction == 'up') {
+    //     $('header a').css('color', '#f2f0ed');
+    //     $('header li').css('color', '#f2f0ed');
+    //   } else if (index == 7 && nextIndex == 3 && direction == 'up') {
+    //     $('header a').css('color', '#f2f0ed');
+    //     $('header li').css('color', '#f2f0ed');
+    //   } else {
+    //     $('header a').css('color', 'black');
+    //     $('header li').css('color', 'black');
+    //   }
+    // },
     afterLoad: function(anchorLink, index){
       if(index == 1) {
-        $header.removeClass('active');
+        header.removeClass('active');
       } else {
-        $header.addClass('active');
+        header.addClass('active');
       }
     }
   });
