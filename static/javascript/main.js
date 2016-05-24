3a76944bea7ff39c81d20d2fa4959e8491746274
Dinah Bakeer <dinah.bakeer@gmail.com>
2016-05-24 13:41:57 -0400

    mobile nav complete

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -22,30 +22,37 @@ $(document).ready(function() {
         $('header a').css('color', '#f2f0ed');
         $('header li').css('color', '#f2f0ed');
         $('.event-date').css('color', '#f2f0ed');
+        $('#fp-nav ul li a span').css('background', '#f2f0ed');
       } else if (index == 4 && direction == 'up') {
         $('header a').css('color', '#f2f0ed');
         $('header li').css('color', '#f2f0ed');
         $('.event-date').css('color', '#f2f0ed');
+        $('#fp-nav ul li a span').css('background', '#f2f0ed');
       } else if (index == 5 && nextIndex == 3 && direction == 'up') {
         $('header a').css('color', '#f2f0ed');
         $('header li').css('color', '#f2f0ed');
         $('.event-date').css('color', '#f2f0ed');
+        $('#fp-nav ul li a span').css('background', '#f2f0ed');
       } else if (index == 6 && nextIndex == 3 && direction == 'up') {
         $('header a').css('color', '#f2f0ed');
         $('header li').css('color', '#f2f0ed');
         $('.event-date').css('color', '#f2f0ed');
+        $('#fp-nav ul li a span').css('background', '#f2f0ed');
       } else if (index == 7 && nextIndex == 3 && direction == 'up') {
         $('header a').css('color', '#f2f0ed');
         $('header li').css('color', '#f2f0ed');
         $('.event-date').css('color', '#f2f0ed');
+        $('#fp-nav ul li a span').css('background', '#f2f0ed');
       } else if (index == 8 && nextIndex == 3 && direction == 'up') {
         $('header a').css('color', '#f2f0ed');
         $('header li').css('color', '#f2f0ed');
         $('.event-date').css('color', '#f2f0ed');
+        $('#fp-nav ul li a span').css('background', '#f2f0ed');
       } else {
         $('header a').css('color', 'black');
         $('header li').css('color', 'black');
         $('.event-date').css('color', 'black');
+        $('#fp-nav ul li a span').css('background', 'black');
       }
     },
     afterLoad: function(anchorLink, index){
