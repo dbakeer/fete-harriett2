ba0d926d9b7a825ae520635ae2a42c1afbebce4d
Dinah Bakeer <dinah.bakeer@gmail.com>
2016-05-27 13:54:48 -0400

    transparent header on first index

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -2,7 +2,7 @@ $('html').hide();
 
 var themeArray = [
   'nil',
-  'light',  // fete harriet
+  'transparent',  // fete harriet
   'dark',   // event
   'light',  // details
   'dark',   // hosted by
@@ -41,68 +41,9 @@ $(document).ready(function() {
       console.log('theme', theme);
 
       $header.attr('data-theme', theme);
-
-
-      // if (index == 2 && direction == 'down') {
-      //   $('header a').css('color', '#f2f0ed');
-      //   $('header li').css('color', '#f2f0ed');
-      //   $('.event-date').css('color', '#f2f0ed');
-      //   $('#fp-nav ul li a span').css('background', '#f2f0ed');
-      // } else if (index == 4 && direction == 'up') {
-      //   $('header a').css('color', '#f2f0ed');
-      //   $('header li').css('color', '#f2f0ed');
-      //   $('.event-date').css('color', '#f2f0ed');
-      //   $('#fp-nav ul li a span').css('background', '#f2f0ed');
-      // } else if (index == 5 && nextIndex == 3 && direction == 'up') {
-      //   $('header a').css('color', '#f2f0ed');
-      //   $('header li').css('color', '#f2f0ed');
-      //   $('.event-date').css('color', '#f2f0ed');
-      //   $('#fp-nav ul li a span').css('background', '#f2f0ed');
-      // } else if (index == 6 && nextIndex == 3 && direction == 'up') {
-      //   $('header a').css('color', '#f2f0ed');
-      //   $('header li').css('color', '#f2f0ed');
-      //   $('.event-date').css('color', '#f2f0ed');
-      //   $('#fp-nav ul li a span').css('background', '#f2f0ed');
-      // } else if (index == 7 && nextIndex == 3 && direction == 'up') {
-      //   $('header a').css('color', '#f2f0ed');
-      //   $('header li').css('color', '#f2f0ed');
-      //   $('.event-date').css('color', '#f2f0ed');
-      //   $('#fp-nav ul li a span').css('background', '#f2f0ed');
-      // } else if (index == 8 && nextIndex == 3 && direction == 'up') {
-      //   $('header a').css('color', '#f2f0ed');
-      //   $('header li').css('color', '#f2f0ed');
-      //   $('.event-date').css('color', '#f2f0ed');
-      //   $('#fp-nav ul li a span').css('background', '#f2f0ed');
-      // } else {
-      //   $('header a').css('color', 'black');
-      //   $('header li').css('color', 'black');
-      //   $('.event-date').css('color', 'black');
-      //   $('#fp-nav ul li a span').css('background', 'black');
-      // }
-
-      // if(index == 1) {
-        // $header.removeClass('active');
-        // $('.event-date').removeClass('active');
-        // $('#fp-nav').removeClass('mobile-active');
-      // } else {
         $header.addClass('active');
         $('.event-date').addClass('active');
         $('#fp-nav').addClass('mobile-active');
-      // }
-
-    },
-    afterLoad: function(anchorLink, index){
-      // alert('hi');
-      // var theme = themeArray[index];
-
-      // console.log('theme', theme);
-
-      // $header.attr('data-theme', theme);
-
-
-    },
-    afterRender: function() {
-      console.log('hiii');
     }
   });
 });
