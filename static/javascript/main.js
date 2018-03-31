8f11163a6ac7b9e3384a671ef357d5ffd90a1aa2
dbakeer <dinah.bakeer@gmail.com>
2018-03-30 19:20:44 -0700

    completed tickets section

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
   $('#fullpage').fullpage({
       anchors: ['feteharriett', 'event', 'hosts', 'about', 'tickets', 'sponsors', 'contact'],
       menu: '#menu',
-      responsiveHeight: 1600,
+      // responsiveHeight: 1600,
       autoScrolling: false,
       navigation: true,
       navigationPosition: 'left',
