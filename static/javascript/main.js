b802bb22fa269cd923edd1ccce942aeaee773564
dbakeer <dinah.bakeer@gmail.com>
2018-04-06 18:14:42 -0700

    final updates and made all content editable in the CMS

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,55 +1,54 @@
 var themeArray = [
-  'nil',
-  'transparent',  //fete harriett
-  'red',        //event
-  'light',         //about
-  'light',        //tickets
-  'light',        //auction
-  'light',        //location
-  'light',        //donors
-  'red'         //contacts
+    'nil',
+    'transparent',  //fete harriett
+    'red',          //event
+    'light',        //about
+    'light',        //tickets
+    'light',        //auction
+    'light',        //location
+    'light',        //donors
+    'red'           //contacts
 ];
 
 
 $(document).ready(function() {
 
-  $(".clickable-row").click(function() {
-    window.document.location = $(this).data("href");
-  });
+    $(".clickable-row").click(function() {
+        window.document.location = $(this).data("href");
+    });
 
-  var $header = $('header');
+    var $header = $('header');
 
-  $('#fullpage').fullpage({
-      anchors: ['feteharriett', 'event', 'hosts', 'about', 'tickets', 'sponsors', 'contact'],
-      menu: '#menu',
-      responsiveHeight: 1600,
-      autoScrolling: false,
-      onLeave: function(index, nextIndex, direction) {
-          var theme = themeArray[nextIndex];
-          $header.attr('data-theme', theme);
-          $header.addClass('active');
-          $('.event-date').addClass('active');
-          $('#fp-nav').addClass('mobile-active');
-      }
+    $('#fullpage').fullpage({
+        anchors: ['feteharriett', 'event', 'hosts', 'about', 'tickets', 'sponsors', 'contact'],
+        menu: '#menu',
+        responsiveHeight: 1600,
+        autoScrolling: false,
+        onLeave: function(index, nextIndex, direction) {
+            var theme = themeArray[nextIndex];
+            $header.attr('data-theme', theme);
+            $header.addClass('active');
+            $('.event-date').addClass('active');
+        }
     });
 });
 
 $(document).ready(function(){
     $(window).scroll(function(){
         if ($('li[data-menuanchor="hosts"]').hasClass('active')) {
-            $('li[data-menuanchor="event"]').hide();
-        } else {
-            $('li[data-menuanchor="event"]').show();
+            $('li[data-menuanchor="event"]').addClass('active');
         }
-    })
+    });
 
-    $('.mobile-header .menu').click(function(){
-        $(this).parents('.mobile-header').toggleClass('open');
-    })
+    $('header .menu').click(function(){
+        $(this).parents('header').toggleClass('open');
+        $('body').toggleClass('menu-open');
+    });
 
-    $('.mobile-header li').click(function(){
-        $(this).parents('.mobile-header').removeClass('open');
-    })
+    $('header li').click(function(){
+        $(this).parents('header').removeClass('open');
+        $('body').removeClass('menu-open');
+    });
 
     $('body').fadeIn(800);
-})
+});
